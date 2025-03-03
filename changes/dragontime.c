23f271f1d9adce166f1b211e26f65dbebffa9c80
Ishay Yemini <ishay.yemini@gmail.com>
2025-03-03 18:10:15 +0100

    More changes in dragontime.c

--- changes/dragontime.c
+++ changes/dragontime.c
@@ -915,8 +915,11 @@ int main(int argc, char * argv[])
 	state->curraddr = 0;
 	state->debug_level = 1;
 	state->group = 24;
-	state->delay = 250;
-	state->timeout = 750;
+    // WPA3_ATTACK
+	state->delay = 50;
+	state->timeout = 125;
+	//	state->delay = 250;
+	//	state->timeout = 750;
 	state->num_addresses = 20;
 
 	while ((ch = getopt(argc, argv, "d:hc:v:a:g:r:i:t:o:")) != -1)
@@ -967,6 +970,11 @@ int main(int argc, char * argv[])
 			case 'o':
 				state->output_file = strdup(optarg);
 				break;
+
+            // WPA3_ATTACK
+            case 'n':
+            	state ->num_addresses = atoi(optarg);
+                break;
 
 			case 'h':
 			default:
