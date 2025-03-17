0c5368d9ca22f83b7999f27267b8e39bbfe02073
Ishay Yemini <ishay.yemini@gmail.com>
2025-03-17 17:15:42 +0100

    Copy dragontime

--- generate_user_ctf.sh
+++ generate_user_ctf.sh
@@ -13,6 +13,7 @@ cd ..
 cp "ctf_files/$LEVEL/datareader.py" "$FOLDER/datareader.py"
 cp "ctf_files/$LEVEL/fingerprint.c" "$FOLDER/finger/fingerprint.c"
 cp "ctf_files/$LEVEL/sae.c" "$FOLDER/finger/sae.c"
+cp "ctf_files/$LEVEL/dragontime.c" "$FOLDER/attack/dragontime.c"
 echo "{0215f4f39b38342193918140c4e8c3d4d0d93fa0}" > "$FOLDER/flag"
 
 zip -r "$FOLDER.zip" "$FOLDER"
