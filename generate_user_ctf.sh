a4e017f9ec668142261128c3f15e5101f4f97184
Ishay Yemini <ishay.yemini@gmail.com>
2025-03-19 09:37:54 +0100

    Added more levels and fixed TODO

--- generate_user_ctf.sh
+++ generate_user_ctf.sh
@@ -14,6 +14,9 @@ cp "ctf_files/$LEVEL/datareader.py" "$FOLDER/datareader.py"
 cp "ctf_files/$LEVEL/fingerprint.c" "$FOLDER/finger/fingerprint.c"
 cp "ctf_files/$LEVEL/sae.c" "$FOLDER/finger/sae.c"
 cp "ctf_files/$LEVEL/dragontime.c" "$FOLDER/attack/dragontime.c"
+if [ -e "ctf_files/$LEVEL/find_matches.py" ]; then
+  cp "ctf_files/$LEVEL/find_matches.py" "$FOLDER/find_matches.py"
+fi
 echo "{0215f4f39b38342193918140c4e8c3d4d0d93fa0}" > "$FOLDER/flag"
 
 zip -r "$FOLDER.zip" "$FOLDER"
